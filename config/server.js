1a582e4316f4fce6c21edc675606a168e5d7e937
Iris Ibekwe <iris.ibekwe@civicactions.com>
2021-04-19 07:57:06 -0400

    Initial Commit.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '07f182996458e35ac490dadb64de9468'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7584c279-1133-4746-b981-418e45251eca'
+};
